1279e14fd866d0a00699bb1170b37e483c39c36d
Surasit307 <107473645+Surasit307@users.noreply.github.com>
2022-06-20 13:08:17 +0700

    Add files via upload

---
+++ WebAppService/src/test/java/Postgresql/Test/WebAppService/WebAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Postgresql.Test.WebAppService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
